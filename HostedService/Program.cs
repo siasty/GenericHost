273c45281ef04ae2146444e84cab26e11d34587e
siasty <siasty3@gmail.com>
2018-11-26 14:47:05 +0100

    Dodaj pliki projektów.

---
+++ HostedService/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using HostedService.service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HostedService
+{
+    class Program
+    {
+
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                    .ConfigureHostConfiguration(configHost =>
+                    {
+                        configHost.SetBasePath(Directory.GetCurrentDirectory());
+                        configHost.AddEnvironmentVariables(EnvironmentName.Development);
+                        configHost.AddJsonFile("appsettings.json", optional: true);
+                        configHost.AddCommandLine(args);
+                    })
+                    .ConfigureServices((hostContext, services) =>
+                    {
+                        services.AddHostedService<Test>();
+                    })
+                    .ConfigureLogging((hostContext, configLogging) =>
+                    {
+                        configLogging.AddConsole();
+                    })
+                    .UseConsoleLifetime()
+                    .Build();
+
+            await host.RunAsync();
+        }
+    }
+
+
+}
---
+++ HostedService/service/Test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HostedService.service
+{
+    internal class Test : IHostedService, IDisposable
+    {
+
+        private readonly ILogger _logger;
+        private Timer _timer;
+
+        public Test(ILogger<Test> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Background Service is starting.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("Timed Background Service is working.");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Timed Background Service is stopping.");
+
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
